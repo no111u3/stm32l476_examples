a1b80abb4d91aef11d4f5b968ca67a6cde9cb46c
Boris Vinogradov <no111u3@gmail.com>
2018-04-12 20:14:56 +0300

    Initial commit
    [+] Create build scripts
    [+] Adding empty example (template for future projects)

---
+++ examples/empty/src/runner.cc
@@ -0,0 +1,15 @@
+/* (c) 2018 Boris Vinogradov <no111u3@gmail.com>
+ * System setup and run main thread
+ * @file runner.cc
+ * @author Boris Vinogradov
+ */
+
+#include <runner.hh>
+
+void runner::setup() {
+}
+
+void runner::run() {
+    // Infinity loop
+    while (true);
+}
